0efd206eb04b3d165b2c4b811614141c5ceaf7cb
Manuel Leung Chen <manuel.leungchen@gmail.com>
2021-01-13 20:51:05 -0500

    Creating controllers files

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,44 @@
+// Import the required modules
+const express = require("express");
+// Import the model (burger.js) to use its database functions.
+const burger = require("../models/burger");
+
+const router = express.Router();
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", (req, res) => {
+    burger.all((data) => {
+        const hbsObject = {
+            burgers: data
+        };
+        res.render("index", hbsObject);
+    });
+});
+
+router.get("/api/burgers", (req, res) => {
+    
+    burger.create("burger_name, devoured", [req.body.name, req.body.devoured], (result) => {
+        
+        res.json( { id: result.insertId } );
+    });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+    const condition = `id = ${req.params.id}`;
+    burger.update("devoured", condition, (result) => {
+        (result) => {
+            if (result.changedRows === 0) {
+              // If no rows were changed, then the ID must not exist, so 404
+              return res.status(404).end();
+            }
+            res.status(200).end();
+          }
+    })
+})
+
+// Export routes for server.js to use.
+module.exports = router;
+
+
+
+
