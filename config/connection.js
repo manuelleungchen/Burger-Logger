9e4868aaa3e3881bfe5f6159c72db0f34f71709d
Manuel Leung Chen <manuel.leungchen@gmail.com>
2021-01-14 18:18:58 -0500

    adding JAWSDB connection details

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'sql123456789',
-    database: 'burgers_db'
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: 'j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        port: 3306,
+        user: 'xu6wqsq78nmg1gf9',
+        password: 'jv792ma5kq5x8go5',
+        database: 's1o2gw4sv59jze4g'
+    });
+}
+
 
 // Make connection.
 connection.connect((err) => {
